0ae93e61d5c4f470f6be10644e63b927b5e0eb01
iaas22 <ishaaamin22@gmail.com>
2026-01-23 17:16:44 +0100

    commit final clase 04-Join

---
+++ 04-Join/src/Administracio.java
@@ -0,0 +1,12 @@
+public class Administracio {
+    private int num_poblacio_activa;
+    private Treballador[] poblacio_activa;
+
+    public Administracio() {
+        num_poblacio_activa = 50;
+        poblacio_activa = new Treballador[50];
+        
+      
+    }
+
+}
---
+++ 04-Join/src/Treballador.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+public class Treballador extends Thread {
+    private float sou_anual_brut;
+    private int edat_inici_treball;
+    private int edat_fi_treball;
+    private int edat_actual;
+    private float cobrat;
+    private Random rnd;
+
+    public Treballador(float sou_anual_brut, int edat_inici_treball, int edat_fi_treball, String nom) {
+        super(nom);
+        this.sou_anual_brut = sou_anual_brut;
+        this.edat_inici_treball = edat_inici_treball;
+        this.edat_fi_treball = edat_fi_treball;
+        this.edat_actual = 0;
+        this.cobrat = 0.0f;
+        this.rnd = new Random();
+    }
+
+    public void cobra() {
+        cobrat += sou_anual_brut / 12;
+    }
+
+    public void pagaImpostos() {
+        cobrat -= cobrat * 0.24f;
+    }
+
+    public float getCobrat() {
+        return cobrat;
+    }
+
+    public int getEdat() {
+        return edat_actual;
+    }
+
+    @Override
+    public void run() {
+        for (edat_actual = edat_inici_treball; edat_actual <= edat_fi_treball; edat_actual++) {
+            for (int mes = 1; mes <= 12; mes++) {
+                cobra();
+                pagaImpostos();
+                for (int i = 0; i < rnd.nextInt(1000); i++) {
+                }
+            }
+        }
+    }
+}

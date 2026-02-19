a0454d1abce155b6a4e4753bf29ecf4cac9f1813
iaas22 <ishaaamin22@gmail.com>
2026-02-19 18:36:14 +0100

    commit final Actividad 08-Filosofs_wait

--- 08-Filosofs_wait/src/Filosof.java
+++ 08-Filosofs_wait/src/Filosof.java
@@ -11,4 +11,86 @@ public class Filosof extends Thread {
         this.gana = 0;
     }
 
+    public void menjar() {
+        try {
+            System.out.println(nom + " està menjant");
+            int temps = (int) (Math.random() * 1000 + 1000); // 1s a 2s
+            Thread.sleep(temps);
+            System.out.println(nom + " ha acabat de menjar");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pensar() {
+        try {
+            System.out.println(nom + " està pensant");
+            int temps = (int) (Math.random() * 1000 + 1000); // 1s a 2s
+            Thread.sleep(temps);
+            System.out.println(nom + " ha acabat de pensar");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void agafarForquillaEsquerra() throws InterruptedException {
+        synchronized (forquillaEsquerra) {
+            while (forquillaEsquerra.getPropietari() != Forquilla.LLIURE) {
+                forquillaEsquerra.wait();
+            }
+            forquillaEsquerra.setPropietari(Integer.parseInt(nom.substring(3)));
+            System.out.println(nom + " ha agafat la forquilla esquerra " + forquillaEsquerra.getNumero());
+        }
+    }
+
+    public void agafarForquillaDreta() throws InterruptedException {
+        synchronized (forquillaDreta) {
+            while (forquillaDreta.getPropietari() != Forquilla.LLIURE) {
+                forquillaDreta.wait();
+            }
+            forquillaDreta.setPropietari(Integer.parseInt(nom.substring(3)));
+            System.out.println(nom + " ha agafat la forquilla dreta " + forquillaDreta.getNumero());
+        }
+    }
+
+    public void agafarForquilles() throws InterruptedException {
+        agafarForquillaEsquerra();
+        agafarForquillaDreta();
+    }
+
+    public void deixarForquillaEsquerra() {
+        synchronized (forquillaEsquerra) {
+            forquillaEsquerra.setPropietari(Forquilla.LLIURE);
+            System.out.println(nom + " ha deixat la forquilla esquerra " + forquillaEsquerra.getNumero());
+            forquillaEsquerra.notifyAll();
+        }
+    }
+
+    public void deixarForquillaDreta() {
+        synchronized (forquillaDreta) {
+            forquillaDreta.setPropietari(Forquilla.LLIURE);
+            System.out.println(nom + " ha deixat la forquilla dreta " + forquillaDreta.getNumero());
+            forquillaDreta.notifyAll();
+        }
+    }
+
+    public void deixarForquilles() {
+        deixarForquillaEsquerra();
+        deixarForquillaDreta();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                pensar();
+                agafarForquilles();
+                menjar();
+                deixarForquilles();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
